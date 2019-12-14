4e114ae245cd4acb6280afe646ca3e07632832b9
David Wachlin <davidwachlin@gmail.com>
2019-12-13 22:39:07 -0500

    builds and styles footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,42 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React from "react"
+import { Link } from "gatsby"
 
 import styles from "../css/footer.module.css"
-import links from './constants/links'
-import socialIcons from './constants/social-icons'
-import { FaRProject } from 'react-icons/fa'
+import links from "./constants/links"
+import socialIcons from "./constants/social-icons"
+import { FaRProject } from "react-icons/fa"
 
 const Footer = () => {
-    return (
-        <div>
-            <h1>i am footer</h1>
-        </div>
-    )
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.links}>
+        {links.map((item, index) => {
+          return (
+            <Link key={index} to={item.path}>
+              {item.text}
+            </Link>
+          )
+        })}
+      </div>
+      <div className={styles.icons}>
+        {socialIcons.map((item, index) => {
+          return (
+            <a
+              key={index}
+              href={item.url}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {item.icon}
+            </a>
+          )
+        })}
+      </div>
+      <div className={styles.copyright}>
+        copyright &copy; backroads travel company {new Date().getFullYear()} all rights reserved
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
