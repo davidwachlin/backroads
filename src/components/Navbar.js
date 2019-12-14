b5f5e814b0c641192e07fbe2900210122aeccd08
David Wachlin <davidwachlin@gmail.com>
2019-12-13 21:39:39 -0500

    adds usestate hook and creates toggleNav function

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
 import styles from "../css/navbar.module.css"
 import { FaAlignRight } from "react-icons/fa"
@@ -7,6 +7,10 @@ import socialIcons from "./constants/social-icons"
 import logo from "../images/logo.svg"
 
 const Navbar = () => {
+  const [isOpen, setNav] = useState(false)
+  const toggleNav = () => {
+    setNav(isOpen => !isOpen)
+  }
   return (
     <div>
       <h1>i'm navbar</h1>
