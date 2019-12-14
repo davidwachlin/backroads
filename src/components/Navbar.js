04b8ebb774b628f4a9f8e1bd06b215a7a8aadd96
David Wachlin <davidwachlin@gmail.com>
2019-12-13 21:25:28 -0500

    adds contstants directory and links file

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react"
 import { Link } from "gatsby"
 
 const Navbar = () => {
-    return (
-        <div>
-            <h1>i'm navbar</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1>i'm navbar</h1>
+    </div>
+  )
 }
 
 export default Navbar
