15acc1a18826b238fd1664172c63f77baf262b62
David Wachlin <davidwachlin@gmail.com>
2019-12-14 14:56:06 -0500

    creates simple hero component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/Layout"
-
+import SimpleHero from '../components/SimpleHero'
 
 
 export default () => (
   <Layout>
+    <SimpleHero />
     <div>Hello from backroads</div>
   </Layout>
 )
